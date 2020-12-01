488d0e860eba86b094f7a09c9d471fd65c76a161
Alexander Sääf <alexandersaaf95@gmail.com>
2020-12-01 17:23:42 +0100

    Add day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,35 @@
-#[aoc(day1, part1, Bytes)]
-pub fn part1_bytes(input: &[u8]) -> i32 {
-    input.iter().fold(0, |sum, c| match c {
-        b'(' => sum + 1,
-        b')' => sum - 1,
-        _ => unreachable!(),
-    })
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap()).collect::<Vec<usize>>()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Vec<usize>) -> usize {
+    let target = 2020;
+    let mut found_numbers = vec![0; target];
+    for item in input {
+        if found_numbers[(target - item)] != 0 {
+            return item * (target - item);
+        }
+        found_numbers[*item] = 1;
+    }
+
+    return 1;
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &Vec<usize>) -> usize {
+    let target = 2020;
+    for i in input.iter() {
+        for y in input.iter() {
+            for z in input.iter() {
+                if (i + y + z) == target {
+                    return i * y * z;
+                }
+            }
+        }
+    }
+    return 1;
 }
